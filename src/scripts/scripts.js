d25c0ae2046594968707d17820dc864b49c3e8d2
hceregatti <hceregatti01@gmail.com>
2025-09-17 00:27:15 -0300

    up: finalizing the project

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,7 +1,109 @@
+// checkbox
 const checkbox = document.getElementById("repeat-number");
-const input = document.querySelector(".input-toggle");
 
 checkbox.addEventListener("change", () => {
-  input.classList.toggle("active")
-}); 
+  const toggle = document.querySelector(".input-toggle");
 
+  toggle.classList.toggle("active");
+});
+
+// Sortear
+
+const form = document.getElementById("form");
+const wrapperForm = document.querySelector(".form-wrapper");
+const qtd = document.getElementById("quantity");
+const min = document.getElementById("min");
+const max = document.getElementById("max");
+const formResult = document.querySelector(".result");
+const result = document.querySelector(".result-wrapper");
+const btnRepeat = document.querySelector(".btn-repeat");
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+
+  wrapperForm.classList.add("hidden");
+  formResult.classList.remove("hidden");
+
+  const numbers = randomNumber(
+    Number(qtd.value),
+    Number(min.value),
+    Number(max.value)
+  );
+
+  numbers.forEach((num, index) => {
+    const delay = 4000;
+
+    setTimeout(() => {
+      const div = document.createElement("div");
+      const span = document.createElement("span");
+      const p = document.createElement("p");
+      const divBG = document.createElement("div");
+      const spanBG = document.createElement("span");
+
+      p.classList.add("number-result");
+      divBG.classList.add("wrapper-bg");
+      spanBG.classList.add("bg-result");
+
+      p.textContent = num;
+
+      p.classList.add("animation-number");
+      spanBG.classList.add("animation-bg");
+
+      div.appendChild(span);
+      span.appendChild(p);
+      span.appendChild(divBG);
+      divBG.appendChild(spanBG);
+
+      result.appendChild(div);
+    }, index * delay);
+  });
+};
+
+btnRepeat.addEventListener("click", (e) => {
+  result.innerHTML = "";
+
+  const numbers = randomNumber(
+    Number(qtd.value),
+    Number(min.value),
+    Number(max.value)
+  );
+
+  numbers.forEach((num, index) => {
+    const delay = 4000;
+
+    setTimeout(() => {
+      const div = document.createElement("div");
+      const span = document.createElement("span");
+      const p = document.createElement("p");
+      const divBG = document.createElement("div");
+      const spanBG = document.createElement("span");
+
+      p.classList.add("number-result");
+      divBG.classList.add("wrapper-bg");
+      spanBG.classList.add("bg-result");
+
+      p.textContent = num;
+
+      p.classList.add("animation-number");
+      spanBG.classList.add("animation-bg");
+
+      div.appendChild(span);
+      span.appendChild(p);
+      span.appendChild(divBG);
+      divBG.appendChild(spanBG);
+
+      result.appendChild(div);
+    }, index * delay);
+  });
+});
+
+function randomNumber(qtd, min, max) {
+  let numbers = [];
+
+  for (let i = 0; i < qtd; i++) {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    numbers.push(randomNumber);
+  }
+
+  return numbers;
+}
